41e83e727a309154560198b2e2ec1c77dd7e68e1
Michele delle Noci <michele.dellenoci@gmail.com>
2021-02-10 23:29:09 +0100

    + Add setUp to TestCase

--- xUnit.py
+++ xUnit.py
@@ -1,14 +1,31 @@
-class WasRun:
+class TestCase:
     def __init__(self, name):
-        self.WasRun = None
         self.name = name
-    def testMethod(self):
-        self.WasRun = 1
+    def setUp(self):
+        pass
     def run(self):
+        self.setUp()
         method = getattr(self, self.name)
         method()
 
-test = WasRun("testMethod")
-print (test.WasRun)
-test.run()
-print (test.WasRun)
+class WasRun(TestCase):
+    def __init__(self, name):
+        TestCase.__init__(self, name)
+    def testMethod(self):
+        self.WasRun = 1
+    def setUp(self):
+        self.WasRun = None
+        self.wasSetUp = 1
+
+class TestCaseTest(TestCase):
+    def setUp(self):
+        self.test= WasRun("testMethod");
+    def testRunning(self):
+        self.test.run()
+        assert(self.test.WasRun)
+    def testSetUp(self):
+        self.test.run()
+        assert(self.test.wasSetUp)
+
+TestCaseTest("testRunning").run()
+TestCaseTest("testSetUp").run()
