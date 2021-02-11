a4403d59e3697015e7b726410b511ae190794005
Michele delle Noci <michele.dellenoci@gmail.com>
2021-02-11 09:13:10 +0100

    + Add tearDown to TestCase

--- xUnit.py
+++ xUnit.py
@@ -3,29 +3,30 @@ class TestCase:
         self.name = name
     def setUp(self):
         pass
+    def tearDown(self):
+        pass
     def run(self):
         self.setUp()
         method = getattr(self, self.name)
         method()
+        self.tearDown()
 
 class WasRun(TestCase):
     def __init__(self, name):
         TestCase.__init__(self, name)
     def testMethod(self):
         self.WasRun = 1
+        self.log= self.log + "testMethod "
     def setUp(self):
         self.WasRun = None
-        self.wasSetUp = 1
+        self.log= "setUp "
+    def tearDown(self):
+        self.log= self.log + "tearDown "
 
 class TestCaseTest(TestCase):
-    def setUp(self):
-        self.test= WasRun("testMethod");
-    def testRunning(self):
-        self.test.run()
-        assert(self.test.WasRun)
-    def testSetUp(self):
-        self.test.run()
-        assert(self.test.wasSetUp)
+    def testTemplateMethod(self):
+        test= WasRun("testMethod");
+        test.run()
+        assert("setUp testMethod tearDown " == test.log)
 
-TestCaseTest("testRunning").run()
-TestCaseTest("testSetUp").run()
+TestCaseTest("testTemplateMethod").run()
